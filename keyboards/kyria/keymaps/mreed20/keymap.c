a2ca91d9c5f9181475f3285ceaa777f4bc2b2110
Michael Reed <michael@michaelreed.io>
2020-08-06 11:45:55 -0400

    keymap: Use enum for readability

--- keyboards/kyria/keymaps/mreed20/keymap.c
+++ keyboards/kyria/keymaps/mreed20/keymap.c
@@ -15,6 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
+enum layers {
+    BASE = 0,
+    SYMBOLS,
+    NUMBERS,
+};
+
 /* [_LAYERINDEX] = LAYOUT( */
 /*   _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______, */
 /*   _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______, */
@@ -22,19 +28,19 @@
 /*                              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ */
 /* ), */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [BASE] = LAYOUT(
         _______, KC_Q,        KC_W,        KC_D,        KC_F,    KC_K,                                                               KC_J,     KC_U,     KC_R,           KC_L,          KC_SCLN,        _______,
         _______, KC_A,        KC_S,        KC_E,        KC_T,    KC_G,                                                               KC_Y,     KC_N,     KC_I,           KC_O,          KC_H,           _______,
         _______, ALT_T(KC_Z), CTL_T(KC_X), GUI_T(KC_C), KC_V,    KC_B,    _______,       _______,       _______,      _______,       KC_P,     KC_M,     GUI_T(KC_COMM), CTL_T(KC_DOT), ALT_T(KC_SLSH), _______,
                                            _______,     _______, _______, SFT_T(KC_TAB), LT(1,KC_BSPC), LT(2,KC_SPC), SFT_T(KC_ENT), _______,  _______,  _______
      ),
-	[1] = LAYOUT(
+	[SYMBOLS] = LAYOUT(
         _______, _______,     KC_EXLM, KC_LCBR, KC_RCBR, KC_CIRC,                                        KC_PERC, KC_UNDS, KC_QUOT, KC_PIPE, _______, _______,
         _______, KC_CIRC,     KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,                                        KC_EQL,  KC_MINS, KC_DQUO, KC_SLSH, KC_HASH, _______,
         _______, _______,     KC_AT,   KC_LBRC, KC_RBRC, KC_AMPR, _______, _______,    _______, _______, KC_PLUS, KC_TILD, KC_GRV, KC_BSLS,  _______, _______,
                                        _______, _______, _______, _______, KC_ESC,     _______, _______, _______, _______, _______
     ),
-	[2] = LAYOUT(
+	[NUMBERS] = LAYOUT(
         _______, _______, KC_7, KC_8,    KC_9,    _______,                                        _______, _______, _______, _______, _______, _______,
         _______, KC_0,    KC_1, KC_2,    KC_3,    _______,                                        _______, _______, _______, _______, _______, _______,
         _______, _______, KC_4, KC_5,    KC_6,    _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
