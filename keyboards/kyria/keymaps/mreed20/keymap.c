027543696b621b11879a8e599fd55bfc5777cfd4
Michael Reed <michael@michaelreed.io>
2020-08-14 20:12:57 -0400

    Minimize unhoming of the thumbs

--- keyboards/kyria/keymaps/mreed20/keymap.c
+++ keyboards/kyria/keymaps/mreed20/keymap.c
@@ -42,6 +42,10 @@ enum {
     TAB_PREV = S(C(KC_TAB)),
     TAB_NEXT = C(KC_TAB),
 
+    // Mac window navigation
+    APP_PREV = S(G(KC_TAB)),
+    APP_NEXT = G(KC_TAB),
+
     // command-line editing navigation
     WORD_PREV  = A(KC_LEFT),
     WORD_NEXT  = A(KC_RIGHT),
@@ -57,29 +61,28 @@ enum {
 /* ), */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        _______, KC_Q,  KC_W,  KC_D,    KC_F,    KC_K,                                                                           KC_J,    KC_U,    KC_R,     KC_L,    KC_SCLN,  _______,
-        _______, KC_A,  KC_S,  KC_E,    KC_T,    KC_G,                                                                           KC_Y,    KC_N,    KC_I,     KC_O,    KC_H,     _______,
-        _______, MOD_Z, MOD_X, MOD_C,   MOD_V,   KC_B,    _______,         _______,           _______,          _______,         KC_P,    MOD_M,   MOD_COMM, MOD_DOT, MOD_SLSH, _______,
-                               _______, _______, KC_VOLD, LT(_NAV,KC_TAB), LT(_SYMR,KC_BSPC), LT(_SYML,KC_SPC), LT(_NUM,KC_ENT), KC_VOLU, _______, _______
+        _______, KC_Q,  KC_W,  KC_D,    KC_F,    KC_K,                                                                               KC_J,    KC_U,    KC_R,     KC_L,    KC_SCLN,  _______,
+        _______, KC_A,  KC_S,  KC_E,    KC_T,    KC_G,                                                                               KC_Y,    KC_N,    KC_I,     KC_O,    KC_H,     _______,
+        _______, MOD_Z, MOD_X, MOD_C,   MOD_V,   KC_B,    _______,           _______,           _______,          _______,           KC_P,    MOD_M,   MOD_COMM, MOD_DOT, MOD_SLSH, _______,
+                               _______, _______, KC_VOLD, LT(_NAV,APP_PREV), LT(_SYMR,KC_BSPC), LT(_SYML,KC_SPC), LT(_NUM,APP_NEXT), KC_VOLU, _______, _______
     ),
     [_SYML] = LAYOUT(
         _______, _______, KC_COLN, KC_LABK, KC_RABK, KC_SCLN,                                     _______, _______, _______, _______, _______, _______,
-        _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_AT,                                       _______, _______, KC_EQL,  KC_PLUS, KC_PERC, _______,
-        _______, _______, KC_EXLM, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______, KC_ESC,  _______, _______, _______, _______, _______
+        _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_AT,                                       _______, KC_ENT,  KC_EQL,  KC_PLUS, KC_PERC, _______,
+        _______, _______, KC_EXLM, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, KC_ESC,  _______, _______, _______, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_SYMR] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                                     _______, KC_UNDS, KC_PIPE, KC_QUOT, _______, _______,
-        _______, KC_CIRC, KC_ASTR, KC_AMPR, _______, _______,                                     KC_HASH, KC_TILD, KC_SLSH, KC_DQUO, KC_DLR,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_BSLS, KC_GRV,  _______, _______,
-                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______,   _______,                                     _______, KC_UNDS, KC_PIPE, KC_QUOT, _______, _______,
+        _______, KC_CIRC, KC_ASTR, KC_AMPR, KC_TAB,    _______,                                     KC_HASH, KC_TILD, KC_SLSH, KC_DQUO, KC_DLR,  _______,
+        _______, _______, _______, _______, S(KC_TAB), _______, _______, _______, _______, _______, _______, KC_MINS, KC_BSLS, KC_GRV,  _______, _______,
+                                   _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_NUM] = LAYOUT(
         _______, _______, KC_7, KC_8,    KC_9,    _______,                                     _______,  _______, _______, _______, _______, _______,
         _______, KC_0,    KC_1, KC_2,    KC_3,    _______,                                     _______,  _______, _______, _______, _______, _______,
         _______, _______, KC_4, KC_5,    KC_6,    _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
                                 _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______),
-    /* Idea: split navigation layer into Nav-left and Nav-right */
     [_NAV] = LAYOUT(
         _______, _______,    _______,   _______,   _______,  _______,                                     _______, TAB_PREV, KC_PGUP, TAB_NEXT, _______, _______,
         _______, LINE_START, WORD_PREV, WORD_NEXT, LINE_END, _______,                                     _______, KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT, _______,
